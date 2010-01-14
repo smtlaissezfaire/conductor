14b5066cac8e2b44b385ea12874b2fbcbefbc5fa
Scott Taylor <scott@railsnewbie.com>
2010-01-14 13:33:32 -0500

    Set version to 0.1.0

--- lib/conductor.rb
+++ lib/conductor.rb
@@ -7,7 +7,7 @@ class Conductor
 
   module Version
     MAJOR = 0
-    MINOR = 0
+    MINOR = 1
     TINY  = 0
 
     STRING = "#{MAJOR}.#{MINOR}.#{TINY}"
--- spec/conductor/version_spec.rb
+++ spec/conductor/version_spec.rb
@@ -2,16 +2,16 @@ require "spec_helper"
 
 describe Conductor do
   describe "VERSION" do
-    it "should be at 0.0.0" do
-      Conductor::Version::STRING.should == "0.0.0"
+    it "should be at 0.1.0" do
+      Conductor::Version::STRING.should == "0.1.0"
     end
 
     it "should have major as 0" do
       Conductor::Version::MAJOR.should == 0
     end
 
-    it "should have minor as 0" do
-      Conductor::Version::MINOR.should == 0
+    it "should have minor as 1" do
+      Conductor::Version::MINOR.should == 1
     end
 
     it "should have tiny as 0" do
